9e05e979335c4bdf05613650c8c675c63c1abea8
Noob <41689244+nicholas9698@users.noreply.github.com>
2023-11-22 17:24:12 +0800

    Create index.config.ts

---
+++ src/pages/index/index.config.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: nicho-UJN nicholas9698@outlook.com
+ * @Date: 2023-09-08 10:43:30
+ * @LastEditors: nicho-UJN nicholas9698@outlook.com
+ * @LastEditTime: 2023-10-27 16:37:49
+ * @FilePath: /mini-mwp/src/pages/index/index.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export default definePageConfig({
+  navigationBarTitleText: '',
+  usingComponents: {},
+})
