68b7648a745bec82e660e5601f2f87ff7a2a3fb4
Noob <41689244+nicholas9698@users.noreply.github.com>
2023-11-22 17:23:28 +0800

    Create index.vue

---
+++ src/pages/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view>
+    <image-cropper />
+  </view>
+</template>
+<script>
+import ImageCropper from '../../components/Cropper';
+export default {
+  name: 'Index',
+  components: {
+    ImageCropper
+  }
+}
+</script>
