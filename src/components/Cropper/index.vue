2d80e0d2672a809b52092c0eba5eec4fa999adb7
Noob <41689244+nicholas9698@users.noreply.github.com>
2023-11-22 17:18:18 +0800

    Update index.vue

--- src/components/Cropper/index.vue
+++ src/components/Cropper/index.vue
@@ -1,191 +1,342 @@
-<!--
- * @Author: nicho-UJN nicholas9698@outlook.com
- * @Date: 2023-09-08 10:43:30
- * @LastEditors: nicho-UJN nicholas9698@outlook.com
- * @LastEditTime: 2023-10-27 16:42:41
- * @FilePath: /mini-mwp/src/pages/index/index.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
-  <view class="index">
-    <nut-noticebar
-      text="沐沐解题(小学数学题自动解答) 是基于人工智能模型和文心一言开发的小程序应用，旨在帮助学生理解问题求解思路，增强逻辑推理能力。"
-      wrapable style="text-align: left;"
-    ></nut-noticebar>
-    <view>
-      <nut-textarea v-model:model-value="problem" placeholder="请输入待求解的问题。" autofocus="true" limit-show max-length="512" style="height: 14em;" />
+    <view class="image-cropper-wrapper">
+        <view class="bg_container">
+            <view class="bg_top" style="height: 100px;"></view>
+            <view class="bg_middle">
+                <view class="bg_middle_left"></view>
+                <view class="cut_wrapper" :style="{ width: _cut_width + 'px', height: _cut_height + 'px'}">
+                    <view class="border border-top-left"></view>
+					<view class="border border-top-right"></view>
+					<view class="border border-right-top"></view>
+					<view class="border border-bottom-right"></view>
+					<view class="border border-right-bottom"></view>
+					<view class="border border-bottom-left"></view>
+					<view class="border border-left-bottom"></view>
+					<view class="border border-left-top"></view>
+                </view>
+                <view class="bg_middle_right"></view>
+            </view>
+            <view class="bg_bottom"></view>
+        </view>
+        <image class="img" :src="img" :style="{
+            width: _img_width * scale + 'px',
+            height: _img_height * scale + 'px',
+            top: _img_top - (_img_height * (scale - 1)) / 2  + 'px',
+            left: _img_left - (_img_width * (scale - 1)) /2 + 'px',
+        }" @touchstart="_img_touch_start" @touchmove="_img_touch_move" @touchend="_img_touch_end"/>
+		<IconFont class="btn1" name="success" size="2em" color="#70f3ff" @click="detect"></IconFont>
+		<IconFont class="btn2" name="circle-close" size="2em" color="#fa2c19" @click="cancel"></IconFont>
+		<div class="loading" :style="{visibility: loading}">
+			<IconFont name="loading1" size="25vw" color="#ffffff"></IconFont>
+		</div>
+
+        <canvas type="2d" id="my-canvas" disableScroll="true" class="my-canvas-class" :style="{ top: _canvas_top + 'px' }" ></canvas>
     </view>
-    <view class="btn">
-      <nut-button type='info' color="#7232dd" plain style="margin-right: 2em;" @click="takePhoto()">拍照识别</nut-button>
-      <nut-button type="info" :loading="loading" style="margin-left: 2em;" @click="handleClick('text', msg, true)">开始解答</nut-button>
-    </view>
-    <view>
-      <nut-textarea readonly v-model:model-value="answer" />
-    </view>
-    <nut-toast :msg="msg" v-model:visible="show" :type="type" :cover="cover"/>
-    <nut-popup round="true" position="bottom" closeable :style="{ height: '15%', paddingTop: '15%'}" v-model:visible="showPop" @click-close-icon="onCloseIcon" @click-overlay="onCloseIcon">网络连接失败！</nut-popup>
-    <nut-dialog title="温馨提示" content="亲，授权小程序登录后才能正常使用小程序功能" v-model:visible="visible" @cancel="onCancel" @ok="onOk" />
-    <nut-overlay v-model:visible="overshow" :close-on-click-overlay="false">
-      <div class="display: flex; height: 100%; align-items: center;justify-content: center;">
-        <image-cropper />
-      </div>
-    </nut-overlay>
-    <view>
-      <my-tabbar />
-    </view>
-  </view>
 </template>
-
 <script>
-import { reactive, toRefs } from 'vue';
+import { toRefs, reactive, getCurrentInstance } from 'vue';
 import Taro from '@tarojs/taro';
-import ImageCropper from '../../components/Cropper';
-import MyTabbar from '../../components/Tabbar';
-
+import { IconFont } from '@nutui/icons-vue-taro';
 export default {
-  name: 'Index',
-  components: {
-    MyTabbar,
-    ImageCropper
-  },
-  setup() {
-    Taro.eventCenter.on('cancelOcr', (data) => {
-      state.overshow = false;
-    });
-    Taro.eventCenter.on('ocrResult', (data) => {
-      state.problem = data;
-    })
-    const state = reactive({
-      openid: '',
-      msg: '问题提交成功，请耐心等待～',
-      loading: false,
-      type: 'text',
-      problem: '',
-      answer: '',
-      show: false,
-      showPop: false,
-      cover: false,
-      overshow: false,
-      visible: true
-    });
-    try {
-      var hasUserInfo = Taro.getStorageSync('hasUserInfo');
-      if (hasUserInfo) {
-        state.visible = false;
-        state.openid = Taro.getStorageSync('openid');
-      }
-    } catch (e) {
-      Taro.setStorageSync('hasUserInfo', false);
-      state.visible = true;
-    };
-    const onOk = () => {
-      Taro.login({
-        success: function(res) {
-          if (res.code) {
-            Taro.request({
-              method: 'POST',
-              url: 'https://api.evosimy.top/minilogin',
-              header: {
-                'content-type': 'application/json'
-              },
-              data: {
-                code: res.code
-              },
-              success: function (res) {
-                Taro.setStorageSync('hasUserInfo', true);
-                Taro.setStorageSync('openid', res.data['openid']);
-                state.openid = res.data['openid'];
-              },
-              fail: function (res) {
-                state.showPop = true;
-              }
-            })
-          } else {
-            // console.log('登录失败！' + res.errMsg)
-            state.showPop = true;
-          }
+    name: 'ImageCropper',
+    components: {
+		IconFont
+},
+    setup() {
+        const state = reactive({
+            img: '',
+			loading: 'hidden',
+            _cut_width: 0,
+            _cut_height: 0,
+            _cut_left: 0,
+            _cut_top: 0,
+			_cut_ratio: 4/3,
+            _img_height: 0,
+            _img_width: 0,
+            _img_left: 0,
+            _img_top: 0,
+            _canvas_top: 9999,
+			_window_height: 0,
+			_window_width: 0,
+            scale: 1,
+			max_scale: 2,
+			min_scale: 0.5,
+            _touch_end_flag: true,
+            _touch_potinter_one: true,
+			_hypotenuse_length: 0,
+			ctx: null,
+			_img_touch_relative: [{
+				x: 0,
+				y: 0
+			},
+			{
+				x: 0,
+				y: 0
+			}]
+        });
+		Taro.getSystemInfo({
+			success: function (res) {
+				state._window_height = res.windowHeight;
+				state._window_width = res.windowWidth;
+				let initial_cut_width = Math.floor((state._window_width * 2) / 3);
+				let initial_cut_height = initial_cut_width / state._cut_ratio;
+				if (initial_cut_height >= state._window_height) {
+					initial_cut_height = Math.floor(state._window_height / 2);
+					initial_cut_width = initial_cut_height * state._cut_ratio;
+				}
+				state._cut_width = initial_cut_width;
+				state._cut_height = initial_cut_height;
+				state._cut_top = (state._window_height - state._cut_height) / 2;
+				state._cut_left = (state._window_width - state._cut_width) / 2;
+			}
+		});
+		Taro.eventCenter.on('acceptDataFromOpenedPage', (data) => {
+			state.img = data.img;
+			Taro.getImageInfo({
+				src: state.img,
+				success: function (res) {
+					state._img_width = state._window_width * 0.8;
+					state._img_left = state._window_width * 0.1;
+					state._img_height = 0.8 * (res.height / (res.width / state._window_width));
+					state._img_top = 0.1 * (res.height / (res.width / state._window_width));
+				}
+			});
+        });
+
+		const _draw = () => {
+			let img_width = state._img_width * state.scale;
+			let img_height = state._img_height * state.scale;
+			let distX = state._img_left - (state._img_width * (state.scale - 1)) / 2 - state._cut_left;
+			let distY = state._img_top - (state._img_height * (state.scale - 1)) / 2 - state._cut_top;
+
+			Taro.createSelectorQuery().select('#my-canvas').fields({node: true, size: true}).exec((res) => {
+				const canvas = res[0].node;
+				canvas.width = state._cut_width;
+				canvas.height = state._cut_height;
+				const ctx = canvas.getContext('2d');
+				ctx.translate(distX, distY);
+				ctx.clearRect(0, 0, state._cut_width, state._cut_height);
+				
+				const image = canvas.createImage();
+				image.onload = () => {
+					ctx.drawImage(image, 0, 0, img_width, img_height);
+					Taro.canvasToTempFilePath({
+						canvas: canvas,
+						fileType: 'jpg',
+						quality: 0.96,
+						success: function(res) {
+							const canvasImg = res.tempFilePath;
+							Taro.uploadFile({
+								url: 'https://api.evosimy.top/ocr',
+								filePath: canvasImg,
+								timeout: 60000,
+								name: 'img',
+								formData: {
+									'user': 'test',
+									'openid': Taro.getStorageSync('openid')
+								},
+								success: function (res) {
+									Taro.eventCenter.trigger('ocrResult', res.data);
+									state.loading = 'hidden';
+									state.img = '';
+									Taro.eventCenter.trigger('cancelOcr', null);
+								}
+							});
+						},
+						fail: function(res) {
+							Taro.eventCenter.trigger('ocrResult', 'Canvas画布导出图片失败，请重试!');
+							state.loading = 'hidden';
+							state.img = '';
+							Taro.eventCenter.trigger('cancelOcr', null);
+						}
+				})
+				}
+				image.src = state.img;
+			})
+		}
+
+        const _img_touch_start = (e) => {
+            state._touch_end_flag = false;
+            if (e.touches.length === 1) {
+                state._touch_potinter_one = true;
+				state._img_touch_relative[0] = {
+					x: e.touches[0].clientX - state._img_left,
+					y: e.touches[0].clientY - state._img_top
+				}
+            } else {
+				state._touch_potinter_one = false;
+				let width = Math.abs(e.touches[0].clientX - e.touches[1].clientX);
+				let height = Math.abs(e.touches[0].clientY - e.touches[1].clientY);
+				state._hypotenuse_length = Math.sqrt(
+					Math.pow(width, 2) + Math.pow(height, 2)
+				);
+			}
         }
-      })
-    };
-    const onCloseIcon = () => {
-      Taro.exitMiniProgram();
-    }
-    const onCancel = () => {
-      Taro.exitMiniProgram();
-    };
+		const _img_touch_end = () => {
+			state._touch_end_flag = true;
+		}
 
-    const takePhoto = () => {
-      Taro.navigateTo({
-        url: '/pages/camera/camera',
-        success: function () {
-          Taro.eventCenter.on('acceptDataFromOpenedPage', (data) => {
-            state.overshow = true;
-          });
+		const _img_touch_move = (e) => {
+			if (state._touch_end_flag) {
+				return;
+			}
+			if (e.touches.length === 1 && state._touch_potinter_one) {
+				let left = e.touches[0].clientX - state._img_touch_relative[0].x;
+				let top = e.touches[0].clientY - state._img_touch_relative[0].y;
+				setTimeout(() => {
+					state._img_left = left;
+					state._img_top = top;
+				}, 0);
+			} else if (e.touches.length >=2 && !state._touch_potinter_one) {
+				// 双指放大
+				let width = Math.abs(e.touches[0].clientX - e.touches[1].clientX);
+				let height = Math.abs(e.touches[0].clientY - e.touches[1].clientY);
 
+				let new_hypotenuse_length = Math.sqrt(
+					Math.pow(width, 2) + Math.pow(height, 2)
+				);
+				let newScale = state.scale * (new_hypotenuse_length / state._hypotenuse_length);
+				newScale = newScale > state.max_scale || newScale < state.min_scale ? state.scale : newScale;
+				state._hypotenuse_length = new_hypotenuse_length;
+				setTimeout(() => {
+					state.scale = newScale;
+				}, 0);
+			}
+		}
+		const detect = () => {
+			state.loading = 'visible';
+			_draw();
+		}
+		const cancel = () => {
+			state.loading = 'hidden';
+			state.img = '';
+			Taro.eventCenter.trigger('cancelOcr', null);
+		}
+        return {
+            ...toRefs(state),
+            _img_touch_start,
+			_img_touch_end,
+			_img_touch_move,
+			detect,
+			cancel
         }
-      })
-    };
-
-    const handleClick = (type, msg, cover = false) => {
-      state.show = true;
-      state.loading = true;
-      state.msg = msg;
-      state.type = type;
-      state.cover = cover;
-      Taro.request({
-        url: 'https://api.evosimy.top/reason',
-        timeout: 60000,
-        method: "POST",
-        data: {
-          problem: state.problem,
-          openid: state.openid
-        },
-        header: {
-          'content-type': 'application/json'
-        },
-        success: function (res) {
-          var answer = '等式：' + res.data['expression'] + '\n\n计算结果：' + res.data['values'] + '\n\n问题解释：\n' + res.data['explanation'];
-          state.answer = answer.replace(/(\n|\r|\r\n|↵)/g, '<br/>');
-          state.loading = false;
-          Taro.request({
-            url: 'https://api.evosimy.top/getrecord',
-            method: 'POST',
-            data: {
-              openid: state.openid
-            },
-            header: {
-              'content-type': 'application/json'
-            },
-            success: function (res) {
-              Taro.setStorageSync('dataRecord', res.data['data']);
-              Taro.eventCenter.trigger('dataRecordChanged');
-            }
-          })
-        },
-        fail: function (res) {
-          state.loading = false;
-          console.log('failed')
-        },
-      })
-    };
-
-    return {
-      ...toRefs(state),
-      onOk,
-      onCloseIcon,
-      onCancel,
-      handleClick,
-      takePhoto
     }
-  }
 }
 </script>
-
 <style lang="scss">
-.index {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+.image-cropper-wrapper {
+	width: 100vw;
+	height: 100vh;
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 1;
+	.bg_container {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		pointer-events: none;
+		.bg_top {
+			flex: 1;
+			background-color: rgba(0, 0, 0, 0.3);
+		}
+		.bg_middle {
+			display: flex;
+			.bg_middle_left,
+			.bg_middle_right {
+				flex: 1;
+				background-color: rgba(0, 0, 0, 0.3);
+			}
+			.cut_wrapper {
+				position: relative;
+				.border {
+					background-color: rgba(255, 255, 255, 0.4);
+					position: absolute;
+				}
+				.border-top-left {
+					height: 4px;
+					top: -4px;
+					left: -4px;
+					width: 30rpx;
+				}
+				.border-left-top {
+					height: 30rpx;
+					top: -4px;
+					left: -4px;
+					width: 4px;
+				}
+				.border-top-right {
+					top: -4px;
+					right: -4px;
+					width: 30rpx;
+					height: 4px;
+				}
+				.border-right-top {
+					top: -4px;
+					right: -4px;
+					height: 30rpx;
+					width: 4px;
+				}
+				.border-right-bottom {
+					bottom: -4px;
+					right: -4px;
+					height: 30rpx;
+					width: 4px;
+				}
+				.border-bottom-right {
+					width: 30rpx;
+					height: 4px;
+					bottom: -4px;
+					right: -4px;
+				}
+				.border-left-bottom {
+					height: 30rpx;
+					width: 4px;
+					bottom: -4px;
+					left: -4px;
+				}
+				.border-bottom-left {
+					width: 30rpx;
+					height: 4px;
+					bottom: -4px;
+					left: -4px;
+				}
+			}
+		}
+		.bg_bottom {
+			flex: 1;
+			background-color: rgba(0, 0, 0, 0.3);
+		}
+	}
+	.img {
+		position: absolute;
+		z-index: -1;
+		backface-visibility: hidden;
+		transform-origin: center center;
+	}
+	.btn1 {
+		bottom: 10rpx;
+		right: calc(50vw - 250rpx);
+		position: absolute;
+		z-index: 0;
+	}
+	.btn2 {
+		bottom: 10rpx;
+		left: calc(50vw - 250rpx);
+		position: absolute;
+		z-index: 0;
+	}
+	.loading {
+		height: 25vw;
+		width: 25vw;
+		position: absolute;
+		left: 37.5vw;
+		top: calc(50vh - 12.5vw - 150rpx);
+	}
+	.my-canvas-class {
+		position: absolute;
+	}
 }
 </style>
