c49dcd6cb95d1af92f3348b723aaaac713edec02
Noob <41689244+nicholas9698@users.noreply.github.com>
2023-11-22 17:19:30 +0800

    Create index.config.ts

---
+++ src/components/Cropper/index.config.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: nicho-UJN nicholas9698@outlook.com
+ * @Date: 2023-10-13 16:42:11
+ * @LastEditors: nicho-UJN nicholas9698@outlook.com
+ * @LastEditTime: 2023-10-13 16:42:56
+ * @FilePath: /mini-mwp/src/components/Cropper/index.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export default {
+    "component": true
+}
